56eb72fa20cee5af309f56db15eeaa0272422366
norm-ideal <idehara.n@gmail.com>
2019-07-22 13:03:58 +0900

    first commit to Projection

--- Assets/CutObject.cs
+++ Assets/CutObject.cs
@@ -29,36 +29,32 @@ public class CutObject : MonoBehaviour
 		Mesh mesh = v.GetComponent<MeshFilter>().mesh;
 		Vector3[] vertices = mesh.vertices;
 		Vector3[] normals = mesh.normals;
-		int[] triangles = mesh.triangles;
+//		int[] triangles = mesh.triangles;
 		//convert the vertices to global coordinate
 		for(int i = 0; i < vertices.Length; i++ )
 			vertices[i] = v.transform.TransformPoint(vertices[i]);
 
-		// get the information from cutting plane
-		Vector3 n = cutPlane.transform.TransformVector(cutPlane.GetComponent<MeshFilter>().mesh.normals[0]);
+		// get the global information from cutting plane
+		Vector3 n = cutPlane.transform.TransformDirection(cutPlane.GetComponent<MeshFilter>().mesh.normals[0]);
 		Vector3 p = cutPlane.transform.position;
 
-
-		List<int> newTriangles = new List<int>();
-
-		for(var i = 0; i < triangles.Length; i+=3)
+		for(int i = 0; i < vertices.Length; i++)
 		{
-			float s0, s1, s2;
-			s0 = Vector3.Dot( vertices[ triangles[i]  ]-p, n );
-			s1 = Vector3.Dot( vertices[ triangles[i+1]]-p, n );
-			s2 = Vector3.Dot( vertices[ triangles[i+2]]-p, n );
-			// if all the three points are under the plane, keep it
-			if( s0 <= 0 && s1 <= 0&& s2 <= 0 )
+			Vector3 vp = vertices[i];	// already converted to global
+			float s = Vector3.Dot( vp-p, n );
+			if( s > 0 )
 			{
-				newTriangles.Add( triangles[i] );
-				newTriangles.Add( triangles[i+1] );
-				newTriangles.Add( triangles[i+2] );
+				Vector3 q = vp - p - s * n + p;	// still in global coord.
+				vertices[i] = q;
+				normals[i] = v.transform.InverseTransformDirection( n );
 			}
-			// otherwise, delete it
 		}
 
-		// https://stackoverflow.com/questions/1367504/converting-listint-to-int
-		// update the triangle array
-		mesh.triangles = newTriangles.ToArray();
+		//convert the vertices to global coordinate
+		for(int i = 0; i < vertices.Length; i++ )
+			vertices[i] = v.transform.InverseTransformPoint(vertices[i]);
+
+		mesh.vertices = vertices;
+		mesh.normals = normals;
 	}
 }
